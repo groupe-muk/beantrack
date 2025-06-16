cc5cbd885055ef829b7ba8d02ad0ba97002f66b2
Magezi-Eunice <eunicjasmine84@gmail.com>
2025-06-16 15:04:05 +0300

    Readjusted fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ export default {
         extend: {
             fontFamily: {
                 
-                sans: ['Roboto Slab', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
-                serif: ['Work Sans', 'serif'],
+                sans: ['Work Sans', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+                serif: ['Inter', 'serif'],
             },
         },
     },
