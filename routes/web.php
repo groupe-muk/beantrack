11a664ae736d70be66bab00c6aa1607296775315
Gideon Ethan Sempijja <114328044+giddy-e23@users.noreply.github.com>
2025-06-16 18:42:42 +0300

    Fix error in web.php

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::view('dashboard', 'dashboard')
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-});
+
 Route::get('/', function(){
     return view('onboarding');
 });
