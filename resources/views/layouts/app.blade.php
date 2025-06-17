0f19838503aad3752836f7ec5e4f7bda28e164c7
Magezi-Eunice <eunicjasmine84@gmail.com>
2025-06-17 21:31:54 +0300

    Created reuseable table card

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
         <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@9.0.3"></script>
         @stack('scripts')
     </body>
 </html>
