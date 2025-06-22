3a247e818f9493789f13d342df17d3800fad92da
Sunshine757 <nabwamilydia5@gmail.com>
2025-06-22 21:45:43 +0300

    finished setting up the inventory UI view

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,3 +6,21 @@ abstract class Controller
 {
     //
 }
+
+
+use Illuminate\Http\Request;
+use App\Models\Inventory;
+
+class InventoryController extends Controller
+{
+    public function index()
+    {
+        return Inventory::all();
+    }
+
+    public function store(Request $request)
+    {
+        $item = Inventory::create($request->all());
+        return response()->json($item, 201);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\columnChartController;
+use App\Http\Controllers\InventoryController;
 
 Route::get('/sample', [columnChartController::class, 'showColumnChart'])->name('column.chart');
 
@@ -61,4 +62,8 @@ Route::middleware(['auth'])->group(function () {
 require __DIR__.'/auth.php';
  
 
+
+Route::get('/inventory',function(){
+    return view('Inventory.inventory');
+});
 
