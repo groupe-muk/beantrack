c90d4f72a9e3635be541a009352e2a7e6a2acf5d
Cal3bdev <lwangacalebb@gmail.com>
2025-07-16 03:28:57 +0300

    Added 'Insights' menu item to the dashboard with corresponding icon and link.

--- app/Helpers/MenuHelper.php
+++ app/Helpers/MenuHelper.php
@@ -51,6 +51,11 @@ class MenuHelper
                         'icon' => '<svg class="w-6 h-6 text-dashboard-text-light flex-shrink-0 transition duration-75 group-hover:text-dashboard-text-light dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path></svg>',
                         'label' => 'Reports'
                     ],
+                    [
+                        'href' => '/insights',
+                        'icon' => '<svg class="w-6 h-6 text-dashboard-text-light flex-shrink-0 transition duration-75 group-hover:text-dashboard-text-light dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>',
+                        'label' => 'Insights'
+                    ],
                 ]);
 
             case 'supplier':
